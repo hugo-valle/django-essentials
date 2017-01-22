57fc1256095953bb8c80f58ffa530858933cdb19
Hugo Valle <hugovalle1@weber.edu>
2017-01-22 11:26:39 -0700

    Update detail.html to display album details

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'music'  # for namespace name tags
+
+
 urlpatterns = [
-    # /music
+    # /music/
     url(r'^$', views.index, name="index"),      # open function views.index
 
-    # /music/rec_id
+    # /music/rec_id/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,20 +1,26 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 # Create your views here.
 def index(request):
+    """
+    Get all Album ojbects and display them
+    :param request: http request
+    :return: render of /music/index.html page
+    """
     all_albums = Album.objects.all()    # connect to db and get all recs from table
     context = { 'all_albums' : all_albums, }
     return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-
+    """
+    Detail view for album information
+    :param request: http request
+    :param album_id: album_id or pk
+    :return: render of music/detail.html page
+    """
+    album = get_object_or_404(Album, pk=album_id)   # replaces the try/except statement
     context = { 'album' : album, }
     return render(request, 'music/detail.html', context)
 
