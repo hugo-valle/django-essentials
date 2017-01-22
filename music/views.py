9cfa8f232bab1382e4d7450479c0f6cedbecdbd6
Hugo Valle <hugovalle1@weber.edu>
2017-01-21 17:49:53 -0700

    Use render instead of loader Add new music/detail.html page

--- music/views.py
+++ music/views.py
@@ -1,18 +1,21 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader      # for you html/css templates
 from .models import Album
 
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()    # connect to db and get all recs from table
-    template = loader.get_template("music/index.html")
-    context = {
-        'all_albums' : all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    context = { 'all_albums' : all_albums, }
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    context = { 'album' : album, }
+    return render(request, 'music/detail.html', context)
+
 
