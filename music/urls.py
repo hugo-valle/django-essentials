bbab5ffa7cdd8b980aa3356bdd008365df444a1c
Hugo Valle <hugovalle1@weber.edu>
2017-01-23 22:41:01 -0700

    Create generic views

--- music/urls.py
+++ music/urls.py
@@ -7,11 +7,7 @@ app_name = 'music'  # for namespace name tags
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name="index"),      # open function views.index
-
+    url(r'^$', views.IndexView.as_view(), name="index"),      # open function views.index
     # /music/rec_id/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
-
-    # /music/rec_id/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,47 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album, Song
 
-# Create your views here.
-def index(request):
-    """
-    Get all Album ojbects and display them
-    :param request: http request
-    :return: render of /music/index.html page
-    """
-    all_albums = Album.objects.all()    # connect to db and get all recs from table
-    context = { 'all_albums' : all_albums, }
-    return render(request, 'music/index.html', context)
+
+# Create a generic view use Classes instead of defenitions
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request, album_id):
-    """
-    Detail view for album information
-    :param request: http request
-    :param album_id: album_id or pk
-    :return: render of music/detail.html page
-    """
-    album = get_object_or_404(Album, pk=album_id)   # replaces the try/except statement
-    context = { 'album' : album, }
-    return render(request, 'music/detail.html', context)
-
-
-def favorite(request, album_id):
-    """
-    Select favorite songs
-    :param request: http request
-    :param album_id: album_id or pk
-    :return: render of music/detail.html page
-    """
-    album = get_object_or_404(Album, pk=album_id)   # replaces the try/except statement
-    context = { 'album' : album,}
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        context['error_message'] = "You did not select a valid song"
-        return render(request, 'music/detail.html', context)
-    else:
-        # Update db
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', context)
-
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
