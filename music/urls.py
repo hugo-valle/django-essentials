66aa245e0c55f77c52684f37bdc6f58a0f0482b1
Hugo Valle <hugovalle1@weber.edu>
2017-01-23 21:15:42 -0700

    Add bootstrap to music index page

--- music/urls.py
+++ music/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
 
     # /music/rec_id/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+
+    # /music/rec_id/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name="favorite"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
@@ -24,4 +24,24 @@ def detail(request, album_id):
     context = { 'album' : album, }
     return render(request, 'music/detail.html', context)
 
+
+def favorite(request, album_id):
+    """
+    Select favorite songs
+    :param request: http request
+    :param album_id: album_id or pk
+    :return: render of music/detail.html page
+    """
+    album = get_object_or_404(Album, pk=album_id)   # replaces the try/except statement
+    context = { 'album' : album,}
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        context['error_message'] = "You did not select a valid song"
+        return render(request, 'music/detail.html', context)
+    else:
+        # Update db
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', context)
 
